1231c7715aa0c8af6d8b6fab7e515d450e2bf6fc
Arion-Evans <arionevans@gmail.com>
2019-06-01 19:09:27 +1000

    EDA plots & model selection

---
+++ arion_playground.R
@@ -0,0 +1,89 @@
+# load libraries
+library(data.table)
+library(corrplot)
+library(ggplot2)
+
+# load data
+data = setDT(read.csv("Life Expectancy Data.csv"))
+
+
+# replace/remove NAs
+data = data[!is.na(Life.expectancy)]
+cols = names(data)[!grepl("Country|Status", names(data))]
+data[, (cols) := lapply(.SD, function(x) {ifelse(is.na(x), -1, x)}), .SDcols = cols]
+
+
+fix_plot_text = theme(axis.title = element_text(size = 16),
+                      axis.text = element_text(size = 14),
+                      plot.title = element_text(size = 16))
+
+summary(data)
+head(data)
+
+
+# EDA ---------------------------------------------------------------------
+
+# correlation
+corr = cor(data[, c(2,4:22)], use = "complete.obs")
+corrplot(corr, type = "lower")
+
+# status
+ggplot(data, aes(x = Status, y = Life.expectancy)) + 
+  geom_boxplot( fill = "aquamarine4") +
+  theme_minimal() +
+  fix_plot_text +
+  labs(y = "Life Expectancy", title = "Life Expectancy by Country Status")
+
+
+# status & adult mortality
+ggplot(data, aes(x = Adult.Mortality, y = Life.expectancy, colour = Status)) + 
+  geom_point()+
+  theme_minimal() +
+  fix_plot_text +
+  labs(y = "Life Expectancy", title = "Life Expectancy by Adult Mortality & Country Status",
+       x = "Adult Mortality")
+
+# timeseries by status
+ggplot(data[, mean(Life.expectancy, na.rm = T), .(Year, Status)], aes(x = Year, y = V1, colour = Status, group = Status)) + 
+  geom_point()+
+  geom_line() +
+  theme_minimal() +
+  fix_plot_text +
+  labs(y = "Life Expectancy", title = "Life Expectancy Timeseries by Country Status",
+       x = "Year")
+
+
+
+# models ------------------------------------------------------------------
+
+mod1 = lm(data = data[, -c("Country")], Life.expectancy ~.)
+summary(mod1)
+
+
+
+# model selection ---------------------------------------------------------
+
+
+basemodel = lm(data = data[, -c("Country")], formula = Life.expectancy ~ Alcohol)
+fullmodel = lm(data = data[, -c("Country")], formula = Life.expectancy ~ .)
+
+forward = step(object = basemodel, direction = "forward")
+summary(forward)
+
+backward = step(fullmodel, direction = "backward")
+summary(backward)
+
+stepwise = step(fullmodel, direction = "both")
+summary(stepwise)
+
+
+
+
+
+
+
+
+
+
+
+
